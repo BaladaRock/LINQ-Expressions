3799fa8e8e863e23ad018e33ada2ba95ce4543a3
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-08-22 13:02:45 +0300

    LINQ - initial commit

---
+++ ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    public class ExtensionMethods
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate);
+    }
+}
---
+++ ExtensionMethods_Facts/ExtensionFacts.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ExtensionMethods_Facts
+{
+    public class ExtensionFacts
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
