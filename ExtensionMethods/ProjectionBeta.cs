8651fc6b0e3f7ec4766130d8c0698d306223d2bb
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-09-23 18:25:06 +0300

    LINQ ExtMethods - alternative implementation of Projection class

---
+++ ExtensionMethods/ProjectionBeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    internal class ProjectionBeta
+    {
+        private Func<TSource, TSource, int> ComparisonCriteria<TSource, TKey>(
+                Func<TSource, TKey> selector,
+                IComparer<TKey> sorterComparer)
+                => (x, y) => sorterComparer.Compare(selector(x), selector(y));
+    }
+}
